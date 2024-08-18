3fec7d66bdd87048a2deed223b5fc4db7c523611
jonas07br <jonasrafa07@gmail.com>
2024-08-18 16:34:58 -0300

    simple changes

--- solution/bin/Main.cpp
+++ solution/bin/Main.cpp
@@ -4,21 +4,21 @@
 #include "ListaOrdenada.h"
 #include "FileService.h"
 #include "Termo.h"
+#include "Autocomplete.h"
 
 using namespace std;
 
-bool compareInt(const int &a, const int &b)
-{
-    return a <= b; // Ordenacao crescente
-}
+
 
 int main(){
     cout<<"--------------------------------------"<<endl;
     cout<<"Projeto Final: Recurso de autocompletar"<<endl;
     cout<<"--------------------------------------"<<endl;
-    ListaOrdenada<Termo> lista = FileService::readFile("fortune.txt");
-    cout<<endl;
-    cout<<"--------------------------------------"<<endl;
+    string entrada;
+    int size;
+    cin>>entrada>>size;
+    Autocomplete autocompleter(entrada,size);
+      
     
     
 
